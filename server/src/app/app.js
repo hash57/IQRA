f490607ecc10eea5f25e06ab62da718cc337ad83
Habeeb Shaik <Shaik.Habeeb@healthfirsttech.com>
2024-04-03 18:23:27 +0530

    added vercel.json

--- server/src/app/app.js
+++ server/src/app/app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
+app.use('/', (req,res,next) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 module.exports = app;
